1f884c5d3e63d84748aa1bfcd9cb080675284d99
Jessica SantoSi <jessica@dgin.com.br>
2024-03-20 23:07:35 -0300

    Contador de caracteres simples

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   
+    $('textarea').on('keyup paste', function(){
+    setTimeout(function(){
+        var valorTextarea = $('textarea').val().length,
+            palavras = $('textarea').val().split(' ').length;
+        $('.caracteres').text(valorTextarea);
+        $('.palavras').text(palavras);
+    }, 200);
+   });
+   
+});
